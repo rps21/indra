2668713be962b7eec0b953b7ca330849729e5b07
Robert Sheehan <sheehan.robertp@gmail.com>
2017-09-21 11:23:40 -0400

    Improve phosphorylation coarse graining, add mutation and dimer coarse graining.

---
+++ models/fallahi_eval/dynamical_model/model_building/remove_dimers.py
@@ -0,0 +1,8 @@
+def remove_dimers(stmts_in):
+    stmts_new = stmts_in[:]
+    for st in stmts_in:
+        if len(st.agent_list()) > 1:
+            if st.agent_list()[0].entity_matches(st.agent_list()[1]):
+                stmts_new.remove(st)
+    return stmts_new
+
