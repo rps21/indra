317f93a3279547d9dcdc0a9ec93d23abe5492be1
Robert Sheehan <sheehan.robertp@gmail.com>
2018-08-16 15:04:59 -0400

    Added quotes around species in set concentration, and fixed resetting the concentration after equil. Also changed single quotes around bngl input options to double quotes to pass RuleBender parser

--- models/cardiotox/serine_metabolism_model/2018ModelBuilding/pathFinding/addBNGLParameters.py
+++ models/cardiotox/serine_metabolism_model/2018ModelBuilding/pathFinding/addBNGLParameters.py
@@ -5,41 +5,43 @@ def addSimParamters(method='ode',equil=True,equilSpecies=[],viz=True):
         writeMexfile()\n"""
         if equil:
             for species in equilSpecies:
-                newText+= "setConcentration(%s,0)\n" % species
-            newText+="""simulate({method=>'ode',t_end=>1e7,n_steps=>500,atol=>1e-8,rtol=>1e-8})
-            setConcentration(,1e4)
-            simulate({method=>'ode',t_start=>0,t_end=>36000,n_steps=>1000,atol=>1e-8,rtol=>1e-8});\n"""
+                newText+= 'setConcentration("%s",0)\n' % species
+            newText+='simulate({method=>"ode",t_end=>1e7,n_steps=>500,atol=>1e-8,rtol=>1e-8})\n'
+            for species in equilSpecies:
+                newText+= 'setConcentration("%s",1e4)\n' % species
+            newText+='simulate({method=>"ode",t_start=>0,t_end=>36000,n_steps=>1000,atol=>1e-8,rtol=>1e-8});\n'
         else:
-            newText = "simulate({method=>'ode',t_start=>0,t_end=>36000,n_steps=>1000,atol=>1e-8,rtol=>1e-8});\n"
+            newText = 'simulate({method=>"ode",t_start=>0,t_end=>36000,n_steps=>1000,atol=>1e-8,rtol=>1e-8});\n'
 
     elif method == "ssa":
        newText = "generate_network({overwrite=>1})\n"
        if equil:
             for species in equilSpecies:
-                newText+= "setConcentration(%s,0)\n" % species
-            newText+="""simulate({method=>'ssa',t_end=>1e7,n_steps=>500})
-            setConcentration(,1e4)
-            simulate({method=>'ssa',t_start=>0,t_end=>36000,n_steps=>1000);\n"""
+                newText+= 'setConcentration("%s",0)\n' % species
+            newText+='simulate({method=>"ssa",t_end=>1e7,n_steps=>500})\n'
+            for species in equilSpecies:
+                newText+= 'setConcentration("%s",1e4)\n' % species
+            newText+='simulate({method=>"ssa",t_start=>0,t_end=>36000,n_steps=>1000);\n'
        else:
-            newText+="simulate({method=>'ssa',t_start=>0,t_end=>36000,n_steps=>1000});\n"
+            newText+='simulate({method=>"ssa",t_start=>0,t_end=>36000,n_steps=>1000});\n'
 
     elif method == "nf":
         if equil:
             for species in equilSpecies:
-                newText+= "setConcentration(%s,0)\n" % species
-            newText+="""setConcentration(,0)
-            "simulate({method=>'nf',t_end=>1e7,n_steps=>500})
-            "setConcentration(,1e4)
-            "simulate_({method=>'nf',t_start=>0,t_end=>36000,n_steps=>1000);\n"""
+                newText+= 'setConcentration("%s",0)\n' % species
+            newText+='simulate({method=>"nf",t_end=>1e7,n_steps=>500})'
+            for species in equilSpecies:
+                newText+='setConcentration("%s",1e4)\n' % species
+            newText+='simulate({method=>"nf",t_start=>0,t_end=>36000,n_steps=>1000);\n'
         else:
-            newText = "simulate{method=>'nf',_start=>0,t_end=>36000,n_steps=>1000});\n"
+            newText = 'simulate{method=>"nf",t_start=>0,t_end=>36000,n_steps=>1000});\n'
 
     else:
         print("Invalid Simulation Method Called")
         
     if viz:
-        newText+="visualize({type=>'contactmap'})\n"
-        newText+="visualize({type=>'regulatory',groups=>1,collapse=>1})\n"
+        newText+="""visualize({type=>"contactmap"})
+        visualize({type=>"regulatory",groups=>1,collapse=>1})\n"""
 
     return newText
 
@@ -47,3 +49,6 @@ def addSimParamters(method='ode',equil=True,equilSpecies=[],viz=True):
 #Issue will be defining species for equilibration, observables.
 #Can pass as a string, but not ideal for 
 #Could write a function to parse seed speices based on species name 
+
+
+
